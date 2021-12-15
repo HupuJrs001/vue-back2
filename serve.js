6f6328fe5d9c09124496486df4d2b6bcaaa626dd
丁殷明 <yinming.ding@hand-china.com>
2021-12-16 00:29:20 +0800

    ll

---
+++ serve.js
@@ -0,0 +1,21 @@
+const http = require('http');
+let user = [{
+    id:1,
+    name:'dingyinming'
+},
+    {
+        id:2,
+        name:'caoqiang'
+    }]
+  let server  = http.createServer((req,res)=>{
+        res.setHeader('Access-Control-Allow-origin','*');
+      if(req.url === '/api/users'){
+
+          res.end(JSON.stringify(user))
+      } else {
+          res.end('Not Found')
+      }
+  })
+server.listen(3000,()=>{
+    console.log('后端API接口服务器已经启动')
+})
